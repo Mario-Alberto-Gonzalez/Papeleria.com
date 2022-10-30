4903be3ba6cc6f9b7af09249360f5c7883c00c2b
Mario-Alberto-Gonzalez <113622834+Mario-Alberto-Gonzalez@users.noreply.github.com>
2022-10-30 08:42:29 -0500

    Add files via upload

---
+++ aside.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-aside',
+  templateUrl: './aside.component.html',
+  styleUrls: ['./aside.component.css']
+})
+export class AsideComponent {
+
+  @Output() notificar = new EventEmitter();
+
+  constructor() { }
+
+}
